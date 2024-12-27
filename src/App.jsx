cdccde57213872694e342a1f6848d45961fd101c
Divyanshi1611 <sharmadivyanshi622@gmail.com>
2024-12-28 00:02:37 +0530

    commitchange

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import React from react
+import React from 'react'
 
 function App() {
   return (
     <>
-      <div className="w=full h-screen bg-zinc-900" >
-        <h1 className="text-white text-center text-2xl mt-34">
-          Hello 
-        </h1>
-        
-
-
-
-      </div>
+      
     </>
     
   )
