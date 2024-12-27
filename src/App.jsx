e2c21b48fbe44a9d0cd95c1fdfd195335bc90a66
Divyanshi1611 <sharmadivyanshi622@gmail.com>
2024-12-27 23:26:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from react
+
+function App() {
+  return (
+    <>
+      <div className="w=full h-screen bg-zinc-900" >
+        <h1 className="text-white text-center text-2xl mt-34">
+          Hello 
+        </h1>
+        
+
+
+
+      </div>
+    </>
+    
+  )
+}
+
+export default App
