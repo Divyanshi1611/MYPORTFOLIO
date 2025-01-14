4fac6a8f2488af755aa48094b363527a5b07946c
Divyanshi1611 <sharmadivyanshi622@gmail.com>
2025-01-14 23:35:25 +0530

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
-
-function Home() {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-    </div>
-  );
+import Profile from '../../assets/assets/home.png';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
+import '.home.css';
+const Home = () => {
+  return(
+     <section className="home section grid">
+      <img src={Profile} alt='' className='home__img'/>
+      <div className="home__content">
+        <div className="home__data">
+          <h1 className="home__title">
+            <span>I'm Divyanshi Sharma</span>Student
+          </h1>
+          <p className="home__description">
+          I’m Divyanshi Sharma, a passionate and driven individual currently preparing for placements while actively contributing to the tech and competitive programming community. 
+          As a proud member of the Competitive Programming (CP) team at Google Developer Group (GDG) and an organizer of events like HACK SPRINT, I enjoy fostering innovation and collaboration.
+          </p>
+          <Link to='/about' className='button'>
+             More About Me<span classname="button__icon"><FaArrowRight/></span>
+          </Link>
+        </div>
+         
+      </div>
+     </section>
+  )
 }
 
 export default Home;
